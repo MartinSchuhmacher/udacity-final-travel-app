b7eaca98b6d1fbb3f4f050fac7ead649ff0554a8
MartinSchuhmacher <mschuhmacher@gmx.de>
2021-03-27 13:48:16 +0100

    style: Add results style (mobile)

--- src/client/js/getTrip.js
+++ src/client/js/getTrip.js
@@ -4,20 +4,22 @@ function getTrip(resultsArea, dateDiff, totalData) {
     const pictureElement = `<img class="picture" src="${totalData.picture}">`;
     console.log(totalData);
     const dateDiv = document.createElement('div');
-    dateDiv.innerHTML = `Departure to ${totalData.destination} in ${dateDiff} days.`;
+    dateDiv.innerHTML = 
+        `<h2>Departure to ${totalData.destination} in ${dateDiff} days!</h2>
+         <h3>Here is the 16 days weather forecast:</h3>`;
     resultsArea.insertAdjacentHTML('beforeend', pictureElement);
     for (const weathOne of totalData.weather) {
         const newElement = 
             `<li class="weather-box">
-                ${weathOne.valid_date}
-                max ${weathOne.max_temp}°C
+                ${weathOne.valid_date} |
+                 <img src="https://github.com/MartinSchuhmacher/udacity-final-travel-app/blob/main/src/client/media/icons/${weathOne.weather.icon}.png?raw=true"> | 
+                max ${weathOne.max_temp}°C |
                 min ${weathOne.min_temp}°C
-                <img src="https://github.com/MartinSchuhmacher/udacity-final-travel-app/blob/main/src/client/media/icons/${weathOne.weather.icon}.png?raw=true">
             </li>`;
         newList.insertAdjacentHTML('beforeend', newElement);
     }; 
-    newDiv.appendChild(newList);
     newDiv.appendChild(dateDiv);
+    newDiv.appendChild(newList);
     resultsArea.appendChild(newDiv);
 }
 
